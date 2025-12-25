4dc6cf29113102ef84a53bc5ce7b1bcb694d7f6e
linusbayere <linusbayere@gmail.com>
2025-12-25 07:49:20 -0600

    add readJson helper

--- internal/httpapi/helpers.go
+++ internal/httpapi/helpers.go
@@ -3,6 +3,7 @@ package httpapi
 import (
 	"encoding/json"
 	"errors"
+	"io"
 	"net/http"
 )
 
@@ -20,6 +21,27 @@ func writeJSON(w http.ResponseWriter, status int, data any, headers http.Header)
 	w.WriteHeader(status)
 	_, err = w.Write(js)
 	return err
+}
+
+func readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	maxBytes := 1_048_576 // 1 MB
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	if err := dec.Decode(dst); err != nil {
+		if err == io.EOF {
+			return errors.New("body must not be empty")
+		}
+		return err
+	}
+
+	if err := dec.Decode(&struct{}{}); err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
+	return nil
 }
 
 func errorResponse(w http.ResponseWriter, r *http.Request, status int, message string) {
