25475a87e1da7a0a5bbe4503fddfc1d026f8c378
Linus Bayere <linusbayere@gmail.com>
2025-12-25 06:48:18 -0600

    PM-9: add healthz endpoint + tests (#5)

--- internal/httpapi/healthz.go
+++ internal/httpapi/healthz.go
@@ -1,1 +1,9 @@
 package httpapi
+
+import "net/http"
+
+func (app *Application) healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(`{"status":"ok"}`))
+}
