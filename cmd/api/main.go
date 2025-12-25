3c47b44118227067f9f73369bcee369bebb416fc
Linus Bayere <linusbayere@gmail.com>
2025-12-25 06:09:50 -0600

    PM-8: bootstrap http server + routing scaffold (#4)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,30 @@
-package api
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/linus5304/project-manager-api/internal/httpapi"
+)
+
+func main() {
+	addr := ":4000"
+	if v := os.Getenv("ADDR"); v != "" {
+		addr = v
+	}
+
+	app := httpapi.NewApplication()
+
+	srv := &http.Server{
+		Addr:         addr,
+		Handler:      app.Routes(),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	log.Printf("starting server on %s", addr)
+	log.Fatal(srv.ListenAndServe())
+}
